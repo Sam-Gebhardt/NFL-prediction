ec98e8a4ae00020f4011b99af8b7d56eb4d09642
Sam Gebhardt <samgeb35@gmail.com>
2020-07-30 22:09:58 -0700

    Combined a function

--- stats.py
+++ stats.py
@@ -10,32 +10,7 @@ def graph_accuracy():
     pass
 
 
-def team_accuracy(team: str):
-    """Print accuracy of
-    a specific team"""
-
-    conn = sqlite3.connect('NFL.db')
-    c = conn.cursor()
-
-    c.execute("""SELECT week FROM season_2020 WHERE team = 'CURRENT_WEEK'""")
-    week = c.fetchall()[0][0] + 1
-
-    total, correct = 0, 0
-    for i in range(1, week):
-        c.execute("""SELECT prediction, outcome FROM season_2020 WHERE week = ? AND team = ?""", (i, team))
-        data = c.fetchall()[0]
-
-        for j in data:
-            total += 1
-            if j[0] == j[1]:
-                correct += 1
-
-    conn.close()
-
-    print(f"{team}: {correct} correct out of {total} for {correct / total * 100}%")
-
-
-def accuracy():
+def accuracy(team=""):
     """Print out the accuracy of the model"""
 
     conn = sqlite3.connect('NFL.db')
@@ -44,9 +19,17 @@ def accuracy():
     c.execute("""SELECT week FROM season_2020 WHERE team = 'CURRENT_WEEK'""")
     week = c.fetchall()[0][0] + 1
 
+    name = ""
+    if name:
+        name = f"{team}: "
+
     total, correct = 0, 0
     for i in range(1, week):
         c.execute("""SELECT prediction, outcome FROM season_2020 WHERE week = ?""", (i, ))
+
+        if team:
+            c.execute("""SELECT prediction, outcome FROM season_2020 WHERE week = ? AND team = ?""", (i, team))
+
         data = c.fetchall()[0]
 
         for j in data:
@@ -55,8 +38,7 @@ def accuracy():
                 correct += 1
 
     conn.close()
-
-    print(f"{correct} correct out of {total} for {correct / total * 100}%")
+    print(f"{name}{correct} correct out of {total} for {correct / total * 100}%")
 
 
 # TODO refactor accuracy functions into 1 or call the other
@@ -95,7 +77,7 @@ def main():
                 elif flag == 'a':
                     a = True
 
-                elif flag == 'v':
+                elif flag == 'v':  # visual
                     v = True
 
                 else:
@@ -109,7 +91,7 @@ def main():
         graph_accuracy()
 
     if t_a:
-        team_accuracy(t_a)
+        accuracy(t_a)
 
 
 if __name__ == "__main__":
