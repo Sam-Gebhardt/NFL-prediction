0dce13244a13ffb9f602555cfc390b0072fe0843
Sam Gebhardt <samgeb35@gmail.com>
2020-09-15 14:18:41 -0700

    Added graph

--- stats.py
+++ stats.py
@@ -3,10 +3,50 @@ Display more complex stats.
 """
 
 import sqlite3
+import matplotlib.pyplot as plt
 
 
 def graph_accuracy():
-    pass
+
+    conn = sqlite3.connect('NFL.db')
+    c = conn.cursor()
+
+    c.execute("""SELECT week FROM season_2020 WHERE team = 'CURRENT_WEEK'""")
+    week = c.fetchall()[0][0]
+
+    num = []
+    for i in range(1, week + 1):
+        correct = -1
+        c.execute("""SELECT prediction, outcome FROM season_2020 WHERE week = ?""", (i,))
+        data = c.fetchall()
+
+        for j in data:
+            if not j:
+                continue
+            if j[0] == j[1]:
+                correct += 1
+
+        correct //= 2
+        num.append(correct)
+
+    conn.close()
+
+    weeks = [i for i in range(1, 18)]
+    for i in range(17):
+        if i > len(num) - 1:
+            num.append(0)
+        color = "g"
+        if num[i] < 10:
+            color = "r"
+        plt.bar(weeks[i], num[i], color=color, width=1)
+
+    plt.legend()
+    plt.xlabel('Week')
+    plt.ylabel('Correct')
+
+    plt.title('Accuracy')
+
+    plt.show()
 
 
 def accuracy():
@@ -48,3 +88,4 @@ def accuracy():
 
 if __name__ == "__main__":
     accuracy()
+    graph_accuracy()
