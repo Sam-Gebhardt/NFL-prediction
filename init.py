05bbfc209b4a7bf5015a3f8f4fca38968d420af9
Sam Gebhardt <samgeb35@gmail.com>
2022-07-04 14:36:08 -0700

    Cleaned up init

--- init.py
+++ init.py
@@ -4,235 +4,218 @@ make a table for the season and fill it with the
 schedule for the upcoming season.
 """
 
+# pylint: disable=line-too-long, pointless-string-statement
+
+from time import sleep
 import sqlite3
+import re
 import urllib.request
 import bs4 as bs
-import time
-import re
-from globals import *
+from globals import FULL_TO_ABBREV, NFL_TEAMS, YEAR, CITY_T0_ABBREV
 
 
-if __name__ == "__main__":  # avoids circular imports
-    import predictions
-    import calc_elo
-
-
-def get_schedule(c, conn) -> None:
-
-    for team in NFL_TEAMS:
-
-        source = urllib.request.urlopen(f'https://www.espn.com/nfl/team/schedule/_/name/{team}/season/2021').read()
-        soup = bs.BeautifulSoup(source, features='lxml')
-
-        table = soup.find_all('span')
-        to_text = [i.text for i in table]
-        start, end = 0, len(to_text)
-
-        for i in range(len(to_text)):
-
-            if to_text[i] == '1':
-                start = i
-
-        to_text = to_text[start:end]
-        bye, shift = 0, 0
-
-        for i in range(0, 18):  # populate table with schedule
-
-            opponent = re.sub("[*]", "", to_text[4 + (i * 6) + shift]).strip()
-
-            if not bye and to_text[(i * 6) + 1].isnumeric():
-                shift = -5
-                opponent = "BYE"
-                bye = i + 1
-
-            c.execute("""INSERT INTO season_2021 (week, team, opponent) VALUES (?, ?, ?)""",
-                      (i + 1, team, opponent))
-            print(i + 1, team, opponent)
-
-        c.execute("""UPDATE season_2021 SET bye = ? WHERE team = ? """, (bye, team))
-        time.sleep(3)
-
-    conn.commit()
-
-
-def get_team_name(name: str) -> str:
-    """Returns the team name """
-
-    names = name.split('\n')
-    rank = []
-
-    for i in names:
-        i = re.sub("[0-9.*]", "", i)
-        start, middle = i.find('('), i.find("via")
-
-        if middle != -1:  # pick was traded 2+ times
-            end = i.find(')')
-            i = i[middle + 4:end]
-        elif start != -1:  # pick was traded 1 time
-            i = i[start + 5:middle]
-        
-        rank.append(i.strip())
-
-    return rank
-
-
-def draft_order(c, conn) -> None:
-
-    source = urllib.request.urlopen('https://www.espn.com/nfl/draft2021/story/_/id/31106459/'
-                                    'nfl-draft-order-2021-picks-including-all-259-picks-seven-rounds')
-
-    soup = bs.BeautifulSoup(source, features='lxml')
-    table = soup.find_all("p")
-    ranking = []
-
-    for i in table:
-
-        if i.text[:2] == "1.":
-            ranking = get_team_name(i.text)
-            break
-
-    if len(ranking) != 32:
-        print("Fatal error: Scraping preseason rankings failed")
-        exit(1)
-
-    for j in range(len(ranking)):
-
-        if "San Fran" in ranking[j]:
-            ranking[j] = "San Francisco 49ers"
-
-        c.execute("""UPDATE season_2021 SET loss_rank = ?, win_rank = ?, power = ?, avg_power = ? 
-                    WHERE week = ? AND team = ?""", (j - 32, j + 1, j - 16, j - 16, 1, CONVERT[ranking[j]]))
-
-    conn.commit()
-    c.execute("""SELECT opponent FROM season_2021 WHERE team = ?""", ("nyj", ))
-    jets = c.fetchall()
-
-    c.execute("""SELECT opponent FROM season_2021 WHERE team = ?""", ("lac", ))
-    chargers = c.fetchall()
-
-    """Since there are 2 teams in LA/NY need to
-    have 2 unique locations for both: LA 2 and NY 2"""
-    for i in range(len(jets)):
-
-        if jets[i][0] != "BYE":
-            
-            c.execute("""UPDATE season_2021 SET opponent = ? WHERE team = ? AND week = ?""",
-                    ("New York 2", CONVERSION_CHART[jets[i][0]], i + 1))
-
-        if chargers[i][0] != "BYE":
-
-            c.execute("""UPDATE season_2021 SET opponent = ? WHERE team = ? AND week = ?""",
-                  ("Los Angeles 2", CONVERSION_CHART[chargers[i][0]], i + 1))
-
-    print("Success!")
-
-    conn.commit()
-
-
-def main():
-    conn = sqlite3.connect('NFL.db')
-    c = conn.cursor()
-
-    c.execute("""CREATE TABLE IF NOT EXISTS season_2021 (week int, team text, opponent text, loss_rank int, 
-                win_rank int, outcome text, prediction text, wins int, bye int,  power int, avg_power int, 
-                UNIQUE(week, team))""")
-
-    """Table data:
+"""
+Table data:
     week: The week of the NFL season
     team: The team
     opponent: The opponent of team column
     loss_rank: points lost if an opponent losses to team column
     win_rank: Points gained if an opponent beats team
-    outcome: Did the team win or lose?
     prediction: Are they predicted to win or lose?
-    wins: total team wins
+    outcome: Did the team win or lose?
     bye: the week the team has a bye
+    wins: total team wins
     power: current ELO
-    avg_power: power per week (used to negate difference caused by bye-week)"""
+    avg_power: power per week (used to negate difference caused by bye-week)
+"""
 
-    try:
-        # dummy team that holds the current week on the NFL season
-        c.execute("""INSERT INTO season_2021 (week, team) VALUES (?, ?)""", (1, "CURRENT_WEEK"))
 
-    except sqlite3.IntegrityError:  # Already been initialized
-        print("Database already initialized.")
-        return
+def fix_two_team_problem(cursor: sqlite3.Cursor, conn: sqlite3.Connection) -> None:
+    """
+    Both LA and NY have two teams which makes matching the city name
+    to team name impossible.
 
-    get_schedule(c, conn)
-    draft_order(c, conn)
+        "New York" -> "Jets"
+        "New York" -> "Rams"
 
-    conn.close()
-    
+    Iterate through each LA/NY teams schedule and modify the opponents
+    to have the correct team abbrev
+    """
 
-def mid_season_init(week: int):  # untested: test once season starts
-    """Create the database after the season has already started"""
+    teams = 'lac', 'lar', 'nyg', 'nyj'
+    for team in teams:
 
-    conn = sqlite3.connect('NFL.db')
-    c = conn.cursor()
+        cursor.execute(f"""SELECT week, opponent FROM season_{YEAR} WHERE
+                        team = ?""", (team, ))
+        out = cursor.fetchall()
 
-    c.execute("""UPDATE season_2021 SET week = ? WHERE team = 'CURRENT_WEEK'""", (week, ))
+        for week, opponent in out:
+            if opponent == "BYE WEEK":
+                continue
 
+            """
+            If a New York team plays a LA team it might not be
+            able to tell which team is correct. Notify the user to
+            manually update the entry if it is wrong
+            """
+            if (team[0:2] == 'ny' and opponent[0:2] == 'la') or \
+               (team[0:2] == 'la' and opponent[0:2] == 'ny') or \
+               (team[0:2] == opponent[0:2]) :
+
+                print(f'** {team} might need manual correction at week {week} **')
+
+            cursor.execute(f"""UPDATE season_{YEAR} SET opponent = ? WHERE week = ? AND team = ?""",
+                            (team, week, opponent, ))
+
+        conn.commit()
+
+
+def get_schedule(cursor: sqlite3.Cursor, conn: sqlite3.Connection) -> None:
+    """
+    Get the schedule for the season and put it into the database
+    """
     for team in NFL_TEAMS:
 
-        source = urllib.request.urlopen(f'https://www.espn.com/nfl/team/schedule/_/name/{team}/season/2021').read()
+        source = urllib.request.urlopen(f'https://www.espn.com/nfl/team/schedule/_/name/{team}/season/{YEAR}').read()
         soup = bs.BeautifulSoup(source, features='lxml')
 
-        table = soup.find_all('span')
-        to_text = [i.text for i in table]
+        table = soup.find_all('td', class_='Table__TD')
 
-        for i in range(0, 18):
+        # Hold in a list as it allows filtering out the preseason games
+        # The order of games depends on if the preseason has started yet
+        schedule = []
+        for i in table:
+            schedule.append(i.text)
 
-            possible_bye = False
-            if 12 > i > 2:
-                possible_bye = True
+        start, end = 0, len(schedule) - 1
+        for index, item in enumerate(schedule):
+            if "Regular" in item:
+                start = index
 
-            delta = 6
-            if week > i + 1:  # if the week has been played, delta is 11 due to increased data from ESPN
-                delta = 11
+            if "Preseason" in item and index != 0:
+                end = index
 
-            second = False
-            start = 0
-            end = len(to_text)
+        schedule =  schedule[start:end]
+        # print(schedule)
 
-            for j in range(len(to_text)):
-                # cleanse irreverent data
+        index = 1
+        for i in schedule:
 
-                if to_text[j] == "1":
-                    start = j
-                    second = True
+            # remove '@ ' or 'vs'
+            # * marks a neutral field game
+            i = i[2:].replace('*', '').strip()
 
-                elif second and to_text[j] == "WK":
-                    end = j
-                    break
+            # Deleted first 2 chars: BYE WEEK -> E WEEK
+            if i == "E WEEK":
+                cursor.execute(f"""INSERT INTO season_{YEAR} (week, team, opponent) VALUES (?, ?, ?)""",
+                                (index, team, "BYE WEEK"))
+                cursor.execute(f"""UPDATE season_{YEAR} SET bye = ? WHERE team = ?""",
+                                (index, team, ))
 
-            to_text = to_text[start:end]
+                print(index, team, "BYE_WEEK")
+                index += 1
 
-            shift = 0
-            if possible_bye:
-                shift = -10
+            if i in CITY_T0_ABBREV:
+                cursor.execute(f"""INSERT INTO season_{YEAR} (week, team, opponent) VALUES (?, ?, ?)""",
+                                (index, team, CITY_T0_ABBREV[i], ))
+                print(index, team, CITY_T0_ABBREV[i])
+                index += 1
 
-            outcome = to_text[5 + (i * delta) + shift]   # 5
-            total_wins = to_text[7 + (i * delta) + shift]  # 7
-
-            pattern = re.compile(r"\d+-\d+")
-            match = pattern.search(total_wins)
-
-            if len(outcome) > 1 or not match:
-                outcome = to_text[5 + (week * 11)]
-                total_wins = to_text[7 + (week * 11)]
-                # assume the team is on bye and fix it if they aren't
-
-            c.execute("""UPDATE season_2021 SET outcome = ?, wins = ? WHERE team = ? AND week = ?""",
-                      (outcome, total_wins, team, week + 1))
-
-            time.sleep(2)
+        sleep(3)
 
     conn.commit()
+
+
+def draft_order(cursor: sqlite3.Cursor, conn: sqlite3.Connection) -> None:
+    """
+    Get the draft order to determine initial power rankings for the season
+    """
+
+    url = f'https://www.nfl.com/news/{YEAR}-nfl-draft-order-for-all-seven-rounds'
+    source = urllib.request.urlopen(url)
+
+    soup = bs.BeautifulSoup(source, features='lxml')
+    table = soup.find_all(['div', 'p'], class_='nfl-c-body-part nfl-c-body-part--text')
+
+    pos = 0
+    for index, item in enumerate(table):
+        if "Round 1" in item.text:
+            pos = index + 1
+
+    order = re.split('<br/>|<p>', str(table[pos]))
+    fixed_order = []
+
+    for index, item in enumerate(order):
+
+        # accounts for trading of picks
+        if 'from' in item:
+            pattern = 'from [A-z]+ {1}[A-z]+( {1}[A-z49]+)?'
+        else:
+            pattern = '[A-z]+ {1}[A-z]+( {1}[A-z49]+)?'
+
+        team = re.search(pattern, item)
+        if team:
+            # remove 'from' and 'through' to account for trades
+            fixed_order.append(team[0].replace('from', '').replace('through', '').strip())
+
+    index = 1
+    for item in fixed_order:
+
+        # Remove false positive regex matches
+        if 'div' in item or 'class' in item:
+            continue
+
+        cursor.execute(f"""UPDATE season_{YEAR} SET loss_rank = ?, win_rank = ?, power = ?, avg_power = ?
+                        WHERE week = ? AND team = ?""", (index - 33, index, index - 16, index - 16, 1,
+                        FULL_TO_ABBREV[item]))
+
+        index += 1
+
+
+    conn.commit()
+
+
+def main() -> None:
+    """
+    Driver code
+    """
+
+    conn = sqlite3.connect('NFL.db')
+    cursor = conn.cursor()
+
+    # Make sure Table hasn't already been created
+    # try:
+
+    #     cursor.execute(f"""CREATE TABLE IF NOT EXISTS season_{YEAR} (week int, team text, opponent text,
+    #                     loss_rank int, win_rank int, outcome text, prediction text, wins int, bye int,
+    #                     power int, avg_power int, UNIQUE(week, team))""")
+
+    #     # dummy team that holds the current week on the NFL season
+    #     cursor.execute(f"""INSERT INTO season_{YEAR} (week, team) VALUES (?, ?)""", (1, "CURRENT_WEEK"))
+
+    # except sqlite3.IntegrityError:  # Already been initialized
+    #     print("Database already initialized.")
+    #     return
+
+    # get_schedule(cursor, conn)
+    # draft_order(cursor, conn)
+    fix_two_team_problem(cursor, conn)
+
     conn.close()
 
-    for i in range(1, week + 1):  # update elo based on played weeks
-        calc_elo.main(i)
+    print(f"Database initialized for {YEAR} season")
 
 
 if __name__ == "__main__":
     main()
+
+
+"""
+Notes:
+Winning against #1 = +32
+losing against #1 = -1
+
+Winning against #32 = +1
+losing against #1 = -32
+
+"""
